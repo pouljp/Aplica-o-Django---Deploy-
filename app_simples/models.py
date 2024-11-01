8d57d471f5a98f570203ca0d3e0d76febd8cbb05
João Paulo Nicolas e Lima <pouljp@gmail.com>
2024-10-31 21:38:49 -0300

    Maquina Senac

---
+++ app_simples/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Item(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
