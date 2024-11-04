79c82e1dda496a092c2e59d7ae13b7afb5d22899
João Paulo Nicolas e Lima <pouljp@gmail.com>
2024-11-03 23:35:55 -0300

    Commit inicial do projeto Django e React

---
+++ aprendendo_django/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
